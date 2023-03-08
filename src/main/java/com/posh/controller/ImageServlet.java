5a10036fff7c541fdbefb2cd0c1aeedcd82ac0d7
Mohamed Gomaa <62193989+mohamedgomaa23722@users.noreply.github.com>
2023-03-08 23:05:50 +0200

    product items

---
+++ src/main/java/com/posh/controller/ImageServlet.java
@@ -0,0 +1,28 @@
+package com.posh.controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class ImageServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("image/jpeg");
+        String imageName = req.getParameter("image");
+        String fileDirectory = "";
+        response.setHeader("Content-Disposition", "attachment; filename=\"image.jpg\"");
+        File file = new File(fileDirectory + imageName);
+        try (FileInputStream fileOutputStream = new FileInputStream(file)) {
+            byte[] imageBytes = new byte[(int) file.length()];
+            fileOutputStream.read(imageBytes);
+            response.getOutputStream().write(imageBytes);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
